782e04eccb48ccee2c15eafdba4b79a1155e6cf0
tasmina@hotmail.com <tasmina@hotmail.com>
2025-11-10 15:35:14 +0100

    jokes

--- code_alongs/03_sql_intro/queries.sql
+++ code_alongs/03_sql_intro/queries.sql
@@ -1,1 +1,13 @@
 SELECT * FROM funny_jokes;
+SELECT * FROM funny_jokes;
+
+-- sort by rating
+SELECT * FROM funny_jokes
+
+ORDER BY rating;
+
+
+SELECT * FROM funny_jokes
+
+ORDER BY rating;
+DESC;
---
+++ code_alongs/03_sql_intro/update_joke.sql
@@ -0,0 +1,14 @@
+-- finds joke that we want
+-- use slect for safegurad
+SELECT * FROM funny_jokes WHERE id = 7;
+
+
+UPDATE
+    funny_jokes
+SET
+    rating = 10
+WHERE id = 7;
+
+
+
+
