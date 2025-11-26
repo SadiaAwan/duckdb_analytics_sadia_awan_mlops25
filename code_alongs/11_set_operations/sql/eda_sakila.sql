e945bd9d5f7456bbf88ba3d5aad2f15cec5c017e
tasmina@hotmail.com <tasmina@hotmail.com>
2025-11-26 14:39:09 +0100

    uv start

--- code_alongs/11_set_operations/sql/eda_sakila.sql
+++ code_alongs/11_set_operations/sql/eda_sakila.sql
@@ -1,5 +1,12 @@
+--ex insight
+--actor id 1 > Penelope Guiness
+--plays in movies:academy dinosaur, anaconda confessions, ...
 FROM
     actor;
+
+-- >= 1 more 
+FROM
+    film_actor;
 
 FROM
     film;
@@ -19,6 +26,14 @@ FROM
 FROM
     film_actor;
 
+--bridge table between film and category
+--it holds film-id and category_id
+FROM
+    film_category;
+
+FROM
+    category;
+
 SELECT
     COUNT(*) AS number_movies,
     COUNT(DISTINCT title) AS unique_number_of_tiles
@@ -33,6 +48,19 @@ FROM
 DESC TABLE film_actor;
 
 SELECT
+    c.first_name || ' ' || c.last_name AS name
+FROM
+    customer c;--alias
+
+-- all customer firstname staring with B
+SELECT
+    c.first_name || ' ' || c.last_name AS name
+FROM
+    customer c--alias  
+WHERE
+    c.first_name ILIKE 'b%';
+
+SELECT
     'customer' AS TYPE,
     c.first_name,
     c.last_name
@@ -40,5 +68,4 @@ FROM
     customer c
 WHERE
     c.first_name LIKE 'D%'
-
---More EDAs left for the reader
+    --More EDAs left for the reader
