490129d7bedb478498b7e0ad9caea7196e633f9a
tasmina@hotmail.com <tasmina@hotmail.com>
2025-12-03 13:18:40 +0100

    19_dlt

--- code_alongs/19_dlt/load_sakila_sqlite_duckdb.py
+++ code_alongs/19_dlt/load_sakila_sqlite_duckdb.py
@@ -1,21 +1,35 @@
+# Goal: load from sqlite sakila into sakila.duckdb
+
 import dlt
 from dlt.sources.sql_database import sql_database
 from pathlib import Path
 
-SQLITE_PATH = Path(__file__).parent / "sqlite-sakila.db"
+DATA_PATH = Path(__file__).parent / "data"
+
+print(DATA_PATH)
+
+
 DUCKDB_PATH = Path(__file__).parent / "sakila.duckdb"
 
+#source path
+SQLITE_PATH = DATA_PATH / "sqlite-sakila.db"
 
-source = sql_database(credentials=f"sqlite:///{SQLITE_PATH}", schema="main")
+#target path
+DUCKDB_PATH = DATA_PATH / "sakila.duckdb"
+
+source = sql_database(f"sqlite:///{SQLITE_PATH}", schema="main")
 
 # pipeline writing to a DuckDB file
 pipeline = dlt.pipeline(
-    pipeline_name="sakila_sqlite_to_duckdb",
+    pipeline_name="sakila_sqlite_duckdb",
     destination=dlt.destinations.duckdb(str(DUCKDB_PATH)),
     dataset_name="staging",
 )
+
 
 # run the load (this captures ALL tables automatically)
 load_info = pipeline.run(source, write_disposition="replace")
 
 print(load_info)
+
+
